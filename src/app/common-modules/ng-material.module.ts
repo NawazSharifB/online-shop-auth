206f5c6004128d85a6728635568fa614172ea35e
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-05 12:20:21 +0600

    added ng-material module

---
+++ src/app/common-modules/ng-material.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const MODULES = [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatBadgeModule,
+    MatToolbarModule,
+    MatListModule,
+    MatDividerModule,
+    MatTableModule,
+    MatSnackBarModule,
+    MatMenuModule,
+    MatProgressBarModule
+];
+
+@NgModule({
+
+    imports: [MODULES],
+    exports: [ MODULES]
+  })
+  export class NgMaterialModule { }
