6108c33bee29a8c4d1fd1a1cf8b8ac3b474765f3
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-05 13:16:58 +0600

    removed previous app-shared.module

--- src/app/app-shared/pipes/shorten-title.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShortenTitlePipe } from './shorten-title.pipe';
-
-describe('ShortenTitlePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShortenTitlePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
