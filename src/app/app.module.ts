b4c60312b3ab4143f39f88589908af570bed878f
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-05 12:17:42 +0600

    added shared modules in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgMaterialModule } from './common-modules/ng-material.module';
+import { NavComponent } from './static-components/nav/nav.component';
+import { NotFoundComponent } from './static-components/not-found/not-found.component';
+import { ServerErrorComponent } from './static-components/server-error/server-error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    NgMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
