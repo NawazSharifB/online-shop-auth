4d1cac7847b83590f57246f7f2fe5a4dd4a4fc19
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-05 12:19:59 +0600

    added allProduct url in environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  allProducts: 'https://gist.githubusercontent.com/naieem/c138ff1f12847b2a1b8ad85866426d3d/raw/037825eee126d589ab3e1fff6c3d0119f33f3b5b/Products',
 };
 
 /*
