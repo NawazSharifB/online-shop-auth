03c09accf1bf02853f63113b967bb1460f4e3d97
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-05 14:58:00 +0600

    updated app.routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './static-components/not-found/not-found.component';
+import { ServerErrorComponent } from './static-components/server-error/server-error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'all-products', loadChildren: () => import('./all-products/all-products.module').then(m => m.ProductsModule)},
+  {path: 'product', loadChildren: () => import('./product-details/product-details.module').then(m => m.ProductDetailsModule)},
+  {path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)},
+  {path: 'auth', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)},
+  {path: '', redirectTo: 'all-products', pathMatch: 'full'},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
