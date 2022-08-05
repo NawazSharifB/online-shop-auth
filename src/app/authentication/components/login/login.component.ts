1a4cbc18c3e8500eedbbb1bb006f8ec050c30fca
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-06 04:14:38 +0600

    added login component

--- src/app/authentication/components/login/login.component.ts
+++ src/app/authentication/components/login/login.component.ts
@@ -1,4 +1,6 @@
+import { AuthenticationService } from './../../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  get emailControl(): FormControl {
+    return <FormControl>this.loginForm.get('email');
+  }
+
+  get passwordControl(): FormControl {
+    return <FormControl>this.loginForm.get('password');
+  }
+
+  onLogin(): void {
+    this.authenticationService
+      .loginUser(this.loginForm.value)
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
+
+  private createLoginForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+    });
   }
 
 }
