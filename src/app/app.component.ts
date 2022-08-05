64e81f8636dcc9958641b8338fd69057456e0d9f
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-05 12:16:49 +0600

    modified app.component to init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
+import { DataService } from './app-shared/services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'online-shop';
+
+  private mediaSub!: Subscription;
+
+  constructor(private cdRef: ChangeDetectorRef,
+              private mediaObserver: MediaObserver,
+              public dataService: DataService) {}
+
+  ngOnInit(): void {
+    // localStorage.clear();
+    this.mediaSub = this.mediaObserver.asObservable().subscribe( (change: MediaChange[]) => {
+      // console.log(change[0].mqAlias);
+
+      this.dataService.pageSize.next(change[0].mqAlias);
+    });
+
+    this.dataService.fetchData();
+  }
+
+  ngOnDestroy(): void {
+    this.mediaSub?.unsubscribe();
+  }
 }
