20681b4f8704812263b4989e58b6a4a48b763395
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:25:40 +0600

    updated add-to-cart.component

--- src/app/shared/components/app-add-to-cart/add-to-cart.component.ts
+++ src/app/shared/components/app-add-to-cart/add-to-cart.component.ts
@@ -1,23 +1,43 @@
+import { Subscription } from 'rxjs';
+import { DataService } from './../../services/data.service';
 import { CartService } from '../../../shared/services/cart.service';
 import { Product } from '../../../shared/models/products.model';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-add-to-cart',
   templateUrl: './add-to-cart.component.html',
   styleUrls: ['./add-to-cart.component.scss']
 })
-export class AddToCartComponent {
-  @Input()product!: Product;
+export class AddToCartComponent implements OnInit, OnDestroy {
+  @Input() product!: Product;
+  @Input() inCartProduct!: Product;
 
-  constructor(private cartService: CartService) { }
+  private subscription$ = new Subscription();
+
+  constructor(private cartService: CartService, private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.inCartProduct = this.product;
+    this.subscription$.add(this.dataService.userCartInfo$.subscribe(cartItems => {
+      const productInCart = cartItems.find(item => item._id === this.product._id);
+
+      if (productInCart) {
+        this.inCartProduct = productInCart;
+      }
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
+  }
 
   reduceItem(): void {
-    this.cartService.reduceToCart(this.product);
+    this.cartService.reduceToCart(this.product).subscribe();
   }
 
   addItem(): void {
-    this.cartService.addToCart(this.product);
+    this.cartService.addToCart(this.product).subscribe();
   }
 
 }
