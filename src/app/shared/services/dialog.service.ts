30d10051f8e2c74e16ca06feb719bbca302ee731
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:28:07 +0600

    added dialog.service

---
+++ src/app/shared/services/dialog.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DialogService {
+  isLoading$ = new BehaviorSubject<boolean>(false);
+}
