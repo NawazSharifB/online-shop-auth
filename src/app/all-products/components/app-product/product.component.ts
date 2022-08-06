72af7e26c3c7d34a971b0d109c0fd14187d4f8c9
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:48:20 +0600

    code refactoring for product.component

--- src/app/all-products/components/app-product/product.component.ts
+++ src/app/all-products/components/app-product/product.component.ts
@@ -1,22 +1,20 @@
-import { Product } from '../../../shared/models/products.model';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { AvailableRoutes } from '../../../shared/enums/available-routes';
+import { Product } from '../../../shared/models/products.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent {
   @Input() product!: Product;
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
+  constructor(private router: Router) {}
 
   goDetails(): void {
-    this.router.navigate(['/product/', this.product._id]);
+    this.router.navigate([`/${AvailableRoutes.Product}/`, this.product._id]);
   }
 
 
