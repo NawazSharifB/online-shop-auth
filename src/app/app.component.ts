8d702b0193f84a96619edb66e2d4bb59d9991116
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:21:17 +0600

    updated app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { AuthenticationService } from './authentication/services/authentication.service';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MediaChange, MediaObserver } from '@angular/flex-layout';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { DataService } from './shared/services/data.service';
+import { DialogService } from './shared/services/dialog.service';
+import { mergeMap } from 'rxjs/operators';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,24 +16,28 @@ import { DataService } from './shared/services/data.service';
 export class AppComponent implements OnInit {
   title = 'online-shop';
 
-  private mediaSub!: Subscription;
+  private subscription$ = new Subscription();
 
-  constructor(private cdRef: ChangeDetectorRef,
-              private mediaObserver: MediaObserver,
-              public dataService: DataService) {}
+  constructor(
+    private mediaObserver: MediaObserver,
+    public dataService: DataService,
+    private dialogService: DialogService,
+    private authenticationService: AuthenticationService,
+    private http: HttpClient,
+  ) {}
 
   ngOnInit(): void {
-    // localStorage.clear();
-    this.mediaSub = this.mediaObserver.asObservable().subscribe( (change: MediaChange[]) => {
-      // console.log(change[0].mqAlias);
-
+    this.authenticationService.storeLoginUserInfo();
+    this.subscription$.add(this.mediaObserver.asObservable().subscribe( (change: MediaChange[]) => {
       this.dataService.pageSize.next(change[0].mqAlias);
-    });
-
-    this.dataService.fetchAllProducts().subscribe();
+    }));
   }
 
   ngOnDestroy(): void {
-    this.mediaSub?.unsubscribe();
+    this.subscription$.unsubscribe();
+  }
+
+  get isDataLoading(): Observable<boolean> {
+    return this.dialogService.isLoading$.asObservable();
   }
 }
