069bb43b4970c11df771852fef92c38fdc4dbf2b
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:24:03 +0600

    updated authentication service (login, logout methods)

--- src/app/authentication/services/authentication.service.ts
+++ src/app/authentication/services/authentication.service.ts
@@ -1,3 +1,5 @@
+import { DataService } from './../../shared/services/data.service';
+import { Router } from '@angular/router';
 import { LoggedInUserCredential } from './../../shared/interfaces/logged-in-user-credential';
 import { LoginUserData } from './../../shared/interfaces/login-user-data';
 import { SuccessfulObservableResponse } from './../../shared/interfaces/successful-observable-response';
@@ -12,17 +14,58 @@ import { Observable, tap } from 'rxjs';
 export class AuthenticationService {
   private readonly loggedInUserDataStorageKey = 'loggedInUserInfo';
 
-  constructor(private storageService: StorageService) { }
+  constructor(
+    private storageService: StorageService,
+    private router: Router,
+    private dataService: DataService
+  ) { }
+
+  private get loggedInUserCred(): LoggedInUserCredential  | null {
+    const stringifiedData = localStorage.getItem(this.loggedInUserDataStorageKey);
+
+    if (stringifiedData) {
+      return this.getParsedData(stringifiedData);
+    }
+
+    return null;
+
+  }
 
   registerUser(registrationData: RegisterUserData): Observable<SuccessfulObservableResponse> {
     return this.storageService.registerUser(registrationData);
   }
 
   loginUser(loginData: LoginUserData): Observable<LoggedInUserCredential> {
-    return this.storageService.loginUser(loginData)
-            .pipe(
-              tap(loggedInCred => this.storeLoggedInCred(loggedInCred)),
-            );
+    return this.storageService.loginUser(loginData).pipe(
+      tap(loggedInCred => this.storeLoggedInCred(loggedInCred)),
+      tap(loggedInCred => this.storageService.loggedInUserId$.next(loggedInCred.userId))
+    );
+  }
+
+  redirectIfHasValidLoginInfo(): void {
+    if (this.isUserLoggedIn()) {
+      this.router.navigate(['/all-products']);
+    }
+  }
+
+  isUserLoggedIn(): boolean {
+    return !!this.loggedInUserCred;
+  }
+
+  isLoginInfoValid(): boolean {
+    return (<LoggedInUserCredential>this.loggedInUserCred).expireIn > Date.now();
+  }
+
+  storeLoginUserInfo(): void {
+    if (this.isUserLoggedIn()) {
+      this.storageService.loggedInUserId$.next((<LoggedInUserCredential>this.loggedInUserCred).userId);
+    }
+  }
+
+  onLogout(): void {
+    localStorage.removeItem(this.loggedInUserDataStorageKey);
+    this.storageService.loggedInUserId$.next('');
+    this.dataService.clearAllData();
   }
 
   private storeLoggedInCred(loggedInCred: LoggedInUserCredential): void {
@@ -34,7 +77,7 @@ export class AuthenticationService {
     return JSON.stringify(data);
   }
 
-  // private getParsedData(data: string): any {
-  //   return JSON.parse(data);
-  // }
+  private getParsedData(data: string): any {
+    return JSON.parse(data);
+  }
 }
