0b524c49427d2abf2824f6ae214acbd0b0b57b8e
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:50:57 +0600

    fixed indentation for sorting.component

--- src/app/all-products/components/app-sorting/sorting.component.spec.ts
+++ src/app/all-products/components/app-sorting/sorting.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SortingComponent } from './sorting.component';
 
 describe('SortingComponent', () => {
--- src/app/all-products/components/app-sorting/sorting.component.ts
+++ src/app/all-products/components/app-sorting/sorting.component.ts
@@ -1,24 +1,19 @@
-import { SortByOptions } from './../../../shared/enums/sort-by-options';
 import { Component, OnInit } from '@angular/core';
-import { DataService } from './../../../shared/services/data.service';
 import { SORT_OPTIONS } from '../../../shared/constants/sort-options';
+import { SortByOptions } from '../../../shared/enums/sort-by-options';
 import { DropdownOptions } from '../../../shared/interfaces/dropdown-options';
+import { DataService } from '../../../shared/services/data.service';
 
 @Component({
   selector: 'app-sorting',
   templateUrl: './sorting.component.html',
   styleUrls: ['./sorting.component.scss']
 })
-export class SortingComponent implements OnInit {
+export class SortingComponent {
   availableSortingOptions = SORT_OPTIONS;
   sortBy: DropdownOptions<SortByOptions> = this.dataService.sortBy$.value;
 
-  constructor(
-    private dataService: DataService
-  ) { }
-
-  ngOnInit(): void {
-  }
+  constructor(private dataService: DataService) {}
 
   sortProducts(sortChoice: DropdownOptions<SortByOptions>): void {
     this.sortBy = sortChoice;
