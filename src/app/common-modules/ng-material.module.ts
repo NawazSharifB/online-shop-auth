5ba9ad368c365ac06b0dbf9b60f3af6d3ff274dc
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:24:50 +0600

    removed snackbar module

--- src/app/common-modules/ng-material.module.ts
+++ src/app/common-modules/ng-material.module.ts
@@ -9,7 +9,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -24,7 +23,6 @@ const MODULES = [
     MatListModule,
     MatDividerModule,
     MatTableModule,
-    MatSnackBarModule,
     MatMenuModule,
     MatProgressBarModule
 ];
