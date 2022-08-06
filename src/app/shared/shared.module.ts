a16282b4afad921f9c34016558d883c9f235e37f
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-06 11:23:53 +0600

    update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgMaterialModule } from '../common-modules/ng-material.module';
 import { AddToCartComponent } from './components/app-add-to-cart/add-to-cart.component';
 import { ShortenTitlePipe } from './pipes/shorten-title.pipe';
 
@@ -11,6 +12,7 @@ const EXPORTED_DECLARATIONS = [ShortenTitlePipe, AddToCartComponent];
     imports: [
         CommonModule,
         FlexLayoutModule,
+        NgMaterialModule,
     ],
     exports: [
       EXPORTED_DECLARATIONS,
