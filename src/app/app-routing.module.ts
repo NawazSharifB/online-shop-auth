4b9d8ac7899b38fcc04151564b2b3581c7c8eae2
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:20:44 +0600

    fixed routing indentation and added routeGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,30 @@
+import { LoginGuard } from './shared/guards/login.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './static-components/not-found/not-found.component';
 import { ServerErrorComponent } from './static-components/server-error/server-error.component';
 
 const routes: Routes = [
-  {path: 'all-products', loadChildren: () => import('./all-products/all-products.module').then(m => m.ProductsModule)},
-  {path: 'product', loadChildren: () => import('./product-details/product-details.module').then(m => m.ProductDetailsModule)},
-  {path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)},
-  {path: 'auth', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)},
+  {
+    path: 'all-products',
+    canActivate: [LoginGuard],
+    loadChildren: () => import('./all-products/all-products.module').then(m => m.ProductsModule)
+  },
+  {
+    path: 'product',
+    canActivate: [LoginGuard],
+    canActivateChild: [LoginGuard],
+    loadChildren: () => import('./product-details/product-details.module').then(m => m.ProductDetailsModule)
+  },
+  {
+    path: 'cart',
+    canActivate: [LoginGuard],
+    loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
+  },
   {path: '', redirectTo: 'all-products', pathMatch: 'full'},
   {path: 'server-error', component: ServerErrorComponent},
   {path: '**', component: NotFoundComponent}
