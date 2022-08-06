e1b1afecdbf46df2aa45d28b8b9e7566a64bf910
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:29:20 +0600

    updated nav.component

--- src/app/static-components/nav/nav.component.ts
+++ src/app/static-components/nav/nav.component.ts
@@ -1,6 +1,9 @@
-import { Subscription } from 'rxjs';
-import { DataService } from '../../shared/services/data.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { AuthenticationService } from '../../authentication/services/authentication.service';
+import { DataService } from '../../shared/services/data.service';
+import { DialogService } from '../../shared/services/dialog.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,25 +11,42 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit, OnDestroy {
-
   totalCartItem = 0;
-  dSub!: Subscription;
 
-  constructor(public dataService: DataService) { }
+  private subscription$ = new Subscription();
+
+  constructor(
+    private dialogService: DialogService,
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private dataService: DataService,
+  ) { }
 
   ngOnInit(): void {
-
-    // this.dSub = this.dataService.cart$.subscribe(products => {
-    //   this.totalCartItem = 0;
-    //   products.forEach( product => {
-    //     this.totalCartItem += product.inCart;
-    //   });
-    // });
+    this.subscription$.add(
+      this.dataService.userCartInfo$.subscribe(products => {
+      this.totalCartItem = 0;
+      products.forEach( product => {
+        this.totalCartItem += product.inCart;
+      });
+    }));
   }
 
   ngOnDestroy(): void {
-    this.dSub?.unsubscribe();
+    this.subscription$.unsubscribe();
   }
 
+  get isUserLoggedIn(): boolean {
+    return this.authenticationService.isUserLoggedIn();
+  }
+
+  get isDataLoading(): Observable<boolean> {
+    return this.dialogService.isLoading$.asObservable();
+  }
+
+  logoutUser(): void {
+    this.authenticationService.onLogout();
+    this.router.navigate(['/auth/login']);
+  }
 
 }
