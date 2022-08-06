f0d764ac8ced12b79a21cc0acfd87113788eeee1
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-06 11:26:30 +0600

    updated product model

---
+++ src/app/shared/models/products.model.ts
@@ -0,0 +1,14 @@
+export class Product {
+  inCart = 0;
+  picture = 'assets/pictures/placeholder.jpg';
+
+  constructor(
+      public _id: string,
+      public title: string,
+      public description: string,
+      public price: number,
+      public stock: number,
+  ) {}
+}
+
+
