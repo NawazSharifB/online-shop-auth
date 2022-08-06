6d68820243ca5f11af83f96953d07194c5d452a6
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:37:41 +0600

    cleaned up all-products

--- src/app/all-products/components/app-all-products/all-products.component.spec.ts
+++ src/app/all-products/components/app-all-products/all-products.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AllProductsComponent } from './all-products.component';
 
 describe('AllProductsComponent', () => {
