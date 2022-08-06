ec152908ff6fbde657c8a776c77515f9d8af21c7
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:26:05 +0600

    added login guard

---
+++ src/app/shared/guards/login.guard.ts
@@ -0,0 +1,35 @@
+import { AuthenticationService } from './../../authentication/services/authentication.service';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate, CanActivateChild {
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      if (this.authenticationService.isUserLoggedIn()) {
+        return true;
+      }
+
+      return this.router.parseUrl('/auth/login');
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return this.canActivate(route, state);
+  }
+
+}
