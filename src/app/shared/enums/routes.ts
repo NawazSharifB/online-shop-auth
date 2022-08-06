63a894cceb6bad681ca2761a5483bca0df0f9442
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:46:38 +0600

    added all routes in enum

---
+++ src/app/shared/enums/routes.ts
@@ -0,0 +1,10 @@
+export enum AvailableRoutes {
+  AllProducts = 'all-products',
+  Product = 'product',
+  Cart = 'cart',
+  Auth = 'auth',
+  Login = 'login',
+  Register = 'register',
+  AuthLogin = 'auth/login',
+  AuthRegister = 'auth/register',
+}
