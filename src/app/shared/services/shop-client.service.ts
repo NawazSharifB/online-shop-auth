a1caa2a9c66473edcbc58a6ff59ce6851ee714fc
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 05:28:43 +0600

    model Products on fetch api call

--- src/app/shared/services/shop-client.service.ts
+++ src/app/shared/services/shop-client.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Product } from '../models/products.model';
 
@@ -12,6 +12,9 @@ export class ShopClientService {
   constructor(private http: HttpClient) { }
 
   fetchAllProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(environment.allProducts);
+    return this.http.get<Product[]>(environment.allProducts)
+      .pipe(map(products=> {
+        return products.map(product => new Product(product._id, product.title, product.description, product.price, product.stock));
+      }));
   }
 }
