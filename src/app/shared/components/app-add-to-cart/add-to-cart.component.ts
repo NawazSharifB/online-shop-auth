d29c55c335600811c3a275e66527ff99cecce5fa
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-06 11:24:47 +0600

    updated add-to-cart.component

--- src/app/shared/components/app-add-to-cart/add-to-cart.component.ts
+++ src/app/shared/components/app-add-to-cart/add-to-cart.component.ts
@@ -1,20 +1,16 @@
 import { CartService } from '../../../shared/services/cart.service';
 import { Product } from '../../../shared/models/products.model';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-add-to-cart',
   templateUrl: './add-to-cart.component.html',
   styleUrls: ['./add-to-cart.component.scss']
 })
-export class AddToCartComponent implements OnInit {
-
-  @Input()product: Product;
+export class AddToCartComponent {
+  @Input()product!: Product;
 
   constructor(private cartService: CartService) { }
-
-  ngOnInit(): void {
-  }
 
   reduceItem(): void {
     this.cartService.reduceToCart(this.product);
