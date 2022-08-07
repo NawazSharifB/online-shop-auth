c16c65afd1a66057e448fb122bf488967a0e307c
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 06:47:26 +0600

    import indentation fix

--- src/app/shared/models/cart-product.ts
+++ src/app/shared/models/cart-product.ts
@@ -1,4 +1,4 @@
-import { PurchasedProductData } from "../interfaces/purchase-product-data";
+import { PurchasedProductData } from '../interfaces/purchase-product-data';
 
 export class CartProduct implements PurchasedProductData {
   constructor(public amount: number, public productId: string) {}
--- src/app/shared/models/user-purchase-info.ts
+++ src/app/shared/models/user-purchase-info.ts
@@ -1,5 +1,5 @@
-import { PurchasedProductData } from "../interfaces/purchase-product-data";
-import { PurchasedUserInfo } from "../interfaces/purchased-user-info";
+import { PurchasedProductData } from '../interfaces/purchase-product-data';
+import { PurchasedUserInfo } from '../interfaces/purchased-user-info';
 
 export class UserPurchaseInfo implements PurchasedUserInfo {
   constructor(public userId: string, public purchasedProductData: PurchasedProductData[]) {}
