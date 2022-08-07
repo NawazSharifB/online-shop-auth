e58ce6460ca5092bfd587399d4a0f2403d6f7e5e
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 06:50:07 +0600

    import identation fix in cart.service

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -1,9 +1,9 @@
-import { StorageService } from './storage.service';
-import { Product } from '../models/products.model';
-import { DataService } from './data.service';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { SuccessfulObservableResponse } from '../interfaces/successful-observable-response';
+import { Product } from '../models/products.model';
+import { DataService } from './data.service';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
