ba362ade4559636226fcef15ae5b191628425205
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 07:19:12 +0600

    code refactoring in app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,33 @@
-import { LoginGuard } from './shared/guards/login.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AvailableRoutes } from './shared/enums/available-routes';
+import { LoginGuard } from './shared/guards/login.guard';
 import { NotFoundComponent } from './static-components/not-found/not-found.component';
 import { ServerErrorComponent } from './static-components/server-error/server-error.component';
 
 const routes: Routes = [
   {
-    path: 'all-products',
+    path: AvailableRoutes.AllProducts,
     canActivate: [LoginGuard],
     loadChildren: () => import('./all-products/all-products.module').then(m => m.ProductsModule)
   },
   {
-    path: 'product',
+    path: AvailableRoutes.Product,
     canActivate: [LoginGuard],
     canActivateChild: [LoginGuard],
     loadChildren: () => import('./product-details/product-details.module').then(m => m.ProductDetailsModule)
   },
   {
-    path: 'cart',
+    path: AvailableRoutes.Cart,
     canActivate: [LoginGuard],
     loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)
   },
   {
-    path: 'auth',
+    path: AvailableRoutes.Auth,
     loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
   },
-  {path: '', redirectTo: 'all-products', pathMatch: 'full'},
-  {path: 'server-error', component: ServerErrorComponent},
+  {path: '', redirectTo: AvailableRoutes.AllProducts, pathMatch: 'full'},
+  {path: AvailableRoutes.ServerError, component: ServerErrorComponent},
   {path: '**', component: NotFoundComponent}
 ];
 
