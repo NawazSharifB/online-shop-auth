f1ec06c61427fd0d4b2cc3bcf383c627f59f0858
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 06:20:28 +0600

    cleaned up cart.module

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,28 +1,18 @@
-// import { AppSharedModule } from './../app-shared/app-shared.module';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-
-import { CartComponent } from './components/app-cart/cart.component';
-// import { NgMaterialModule } from '../app-angular-material/app-angular-material.module';
-// import { AppCheckoutModule } from '../app-checkout/app-checkout.module';
-// import { AppAddToCartModule } from '../app-add-to-cart/app-add-to-cart.module';
-import { CheckoutComponent } from './components/app-checkout/checkout.component';
-import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 import { NgMaterialModule } from '../common-modules/ng-material.module';
+import { SharedModule } from '../shared/shared.module';
+import { CartComponent } from './components/app-cart/cart.component';
+import { CheckoutComponent } from './components/app-checkout/checkout.component';
 
 @NgModule({
-    declarations: [CartComponent, CheckoutComponent],
-    imports: [
-        NgMaterialModule,
-        // AppCheckoutModule,
-        SharedModule,
-        // AppAddToCartModule,
-        RouterModule.forChild([
-            {path: '', component: CartComponent}
-        ])
-    ],
-    exports: [
-        // CartComponent
-    ]
-  })
+  declarations: [CartComponent, CheckoutComponent],
+  imports: [
+    NgMaterialModule,
+    SharedModule,
+    RouterModule.forChild([
+      {path: '', component: CartComponent},
+    ]),
+  ],
+})
 export class CartModule {}
