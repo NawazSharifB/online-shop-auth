466c232bc1fbf2f1eb8115972931d1cbc88ea2ca
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 07:10:14 +0600

    code refactoring for server-error.component

--- src/app/static-components/server-error/server-error.component.ts
+++ src/app/static-components/server-error/server-error.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server-error',
   templateUrl: './server-error.component.html',
   styleUrls: ['./server-error.component.scss']
 })
-export class ServerErrorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ServerErrorComponent{}
