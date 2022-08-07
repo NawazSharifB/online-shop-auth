b3cf895dd342a661a02b0894230d25c1db386cc0
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 13:29:24 +0600

    added allProducts url in environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  allProducts: 'https://gist.githubusercontent.com/naieem/c138ff1f12847b2a1b8ad85866426d3d/raw/037825eee126d589ab3e1fff6c3d0119f33f3b5b/Products',
 };
