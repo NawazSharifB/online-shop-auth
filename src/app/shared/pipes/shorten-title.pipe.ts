8302d12f1537b9b641e5b381b4ab0b14f4e86778
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-07 06:49:02 +0600

    indentation fix in shorten-title.pipe

--- src/app/shared/pipes/shorten-title.pipe.ts
+++ src/app/shared/pipes/shorten-title.pipe.ts
@@ -4,16 +4,17 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'shortenTitle'
 })
 export class ShortenTitlePipe implements PipeTransform {
-
   transform(value: string, limit = 20): string {
-    value = value.split(' ')
-                    .map( word => {
-                      return word.charAt(0).toUpperCase() + word.split('').slice(1, word.length).join('');
-                    }).join(' ');
+    value = value
+      .split(' ')
+      .map( word => {
+        return word.charAt(0).toUpperCase() + word.split('').slice(1, word.length).join('');
+      }).join(' ');
+
     if (value.length > limit) {
       value = value.substr(0, limit) + '...';
     }
+
     return value;
   }
-
 }
